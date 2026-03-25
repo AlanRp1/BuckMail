02686023837d90663cd326f43c15bb704ba8f87c
Alan Rp <114629616+AlanRp1@users.noreply.github.com>
2026-03-25 13:40:15 -0300

    Delete backend directory

--- backend/models/Address.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AddressSchema = new mongoose.Schema({
-  address: { type: String, required: true, unique: true },
-  ip: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 3600 } // Auto-delete after 1 hour
-});
-
-module.exports = mongoose.model('Address', AddressSchema);
